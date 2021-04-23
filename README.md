d365b748b544803153a9064d1574799de4e94cde
Natalie Dinin <>
2021-04-23 15:09:38 +0000

    V 0.2

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ My program asks the use to provide 3 numbers ranged from 0-255 to store as an rg
 
 ### v0.2 Updates
 
-*Coming soon*
+My program now adds 5 more colors to the file the user specifies based on what range the initial color is part of. It then prints out the rgb values for all 6 colors in the console.
 
 ### v1.0 Updates
 
@@ -31,11 +31,11 @@ Here is an example of the program running:
 ```
  g++ -std=c++11 *.cpp -o cvp; ./cvp
 Enter a RED color value (Must be between 0 and 255)
-100
+20 
 Enter a GREEN color value (Must be between 0 and 255)
-15
+100
 Enter a BLUE color value (Must be between 0 and 255)
-200
+0
 Please enter a name for the file you will be storing the rgb values in:
 color.svg
 Writing to the file...
@@ -44,24 +44,32 @@ Would you like to read from the file? (y/n)
 y
 Reading from file...
 
-rgb(100,15,200)
-
+rgb1(20,100,0)
 Now that it has been identified which color you would like as a base color, a list of five more colors that do not have problematic color combinations can be generated.
 
-Would you like to add five more colors to color.svg? (Enter y for yes and n for no)
+Would you like to add five more colors to color.svg? (y/n)
 
 y
+Printing out all six nonproblematic colors...
+rgb1(20, 100, 0)
+rgb2(102, 46, 0)
+rgb3(75, 155, 22)
+rgb4(145, 123, 170)
+rgb5(200, 108, 250)
+rgb6(234, 248, 199)
 ```
 
 ## C++ Guide
 
 ### Variables and Data Types
 
-I used the integer variable to store my rgb values I collected from the user as well as the rgb values my program will generate in a later version. I needed to store a non decimal number so I decided the integer data type worked best for this problem.
+I used the integer variable to store my rgb values I collected from the user. I also used an integer variable to store the sum of the 3 rgb values. I needed to store a non decimal number so I decided the integer data type worked best for this problem.
 
-I used the string variable to store the filename that the user chooses. Since that will always be multiple characters, the string data type worked best. I also used a string variable to store the response the user gives when asked whether they want to proceed at two times in the program. 
+I used the string variable to store the filename that the user chooses. Since that will always be multiple characters, the string data type worked best. I used the string variable type to replace text in my function that I use three times to check whether the user has input an integer between 0 and 255. I also used a string variable to store the response the user gives when asked whether they want to proceed at two times in the program. 
 
 I used the fstream variable type because you can't do file input and output without it, at least to my knowledge. 
+
+I used 3 arrays of length 5 to store the rgb values for the 5 colors my program generates. Using arrays made sense because I needed to keep track of a sizeable amount of numbers with a set size. Storing the values in arrays kept the numbers organzed nicely. 
 
 ### Console Input and Output
 
@@ -78,22 +86,29 @@ I used an if statement when I asked whether the user wanted to read the informat
 
 I used an if statement when I asked the user if they would like to add five more colors to their file. I used a decision construct here to give the user an option whether they wanted to continue using the program to generate the features it is supposed to do or whether they would like to quit the program.
 
+In my GenerateColors function, I used an if statement and 5 else if statements to determine which colors should be added to the file along with the color the user input. I used a decision construct here because I needed one of 6 scenarios to happen, depending on the range of the sum of the rgb value the user input.
+
 ### Iteration
 
-I used 3 while loops for when I was collecting the number values for the rgb value of the base color. These loops account for the mistake of the user entering a number outside the specified range and will ask the user to enter a new number until they choose a number in the correct range. This is an effective use of a loop because it will only run if the specifications in the condition hold true. This would not be the case if I had used a do while loop for example. 
+I used a while loop for when I was collecting the number values for the rgb value of the base color. This loop accounts for the mistake of the user entering a number outside the specified range and will ask the user to enter a new number until they choose a number in the correct range. This is an effective use of a loop because it will only run if the specifications in the condition hold true. This would not be the case if I had used a do while loop for example. 
+
+I used a for loop when I was extracting information from my arrays to put them in the file in a clear format showing that they are rgb values of 5 different colors. A for loop was most appropriate because I had a set number of times I wanted to repeat an action. 
 
 ### File Input and Output
 
-I used file input and output effectively in my program. The values I ask for for the base color are stored in a file the user names themselves. To cover my bases, I also have made my program read from that file to make sure the information was stored in it correctly. I remember to open and close the file each time I need to interact with it.  
+I used file input and output effectively in my program. The values I ask for for the base color are stored in a file the user names themselves. The rgb values my program generates are also stored in the same file that the user names themselves. To cover my bases, I also have made my program read from that file to make sure the information was stored in it correctly. I remember to open and close the file each time I need to interact with it.  
 
 
 ### Arrays/Vectors
 
-*Coming in version 0.2*
+I use 3 arrays in my program to keep the red, green, and blue color values that my program generates in an orderly fashion. I call upon specific elements of each array when I need to organize them as rgb values. An array was a more fitting choice than a vector because the set of numbers I am working with is statically sized. 
 
 ### Functions
 
-*Coming in version 0.2*
+My first function, CheckRGBValue, asks the user to enter a number between 0 and 255. If the number they spcify is not in that range, then the program will continue asking them until they enter a value in that range. This funtion then returns an integer value to be stored as the variable red, green, or blue depending on which call to the function the code is running through at the time. The function CheckRGBValue only requires a string to replace text in the cout statements in the function. This is necessary in order to make the function flexible enough to use 3 times. This function uses pass-by-value because it does not need to modify the string parameter. This function is unique because it works for my specific project.
+
+My second function, GenerateColors, modifies 3 unique arrays so that they work with the color the user entered based on the range the sum of the rgb values the user entered. Given 6 possible ranges the sum can fall under, I use 6 decision constructs (one if and five else ifs) to add rgb values whose sum is in the other 5 ranges. This means I needed to pass-by-reference for the 3 arrays specified as parameters for the function, as I needed them to be permanently changed. I used a void return type because this function didnt need to return anything. This function is unique because it works for my specific project.
+
 
 ### Classes
 
--- main.cpp
+++ main.cpp
@@ -3,127 +3,241 @@
 #include <fstream>
 using namespace std;
 
+int CheckRGBValue(string);
+void GenerateColors(int, int (&new_red)[5], int (&new_green)[5], int (&new_blue)[5]);
+
 int main()
 {
 
   int red = 0;
   int green = 0;
   int blue = 0;
-  int red2 = 0;
-  int green2 = 0;
-  int blue2 = 0;
-  int red3 = 0;
-  int green3 = 0;
-  int blue3 = 0; 
-  int red4 = 0;
-  int green4 = 0;
-  int blue4 = 0;
-  int red5 = 0;
-  int green5 = 0;
-  int blue5 = 0;
-  int red6 = 0;
-  int green6 = 0;
-  int blue6 = 0;
+  string RedIndicator = "RED";
+  string GreenIndicator = "GREEN";
+  string BlueIndicator = "BLUE";
 
-  fstream reader;
+  red = CheckRGBValue(RedIndicator);
+  green = CheckRGBValue(GreenIndicator);
+  blue = CheckRGBValue(BlueIndicator);
+
+  ofstream reader;
   string filename;
-  string affirmative;
-  string proceed;
-
-
-  cout << "Enter a RED color value (Must be between 0 and 255)\n";
- cin >> red;
-while (red < 0 || red > 255) {
-    
-   cout << "Sorry, that number is not in the specified range. \n";
-   cout << "Please re-enter a RED color value (Must be between 0 and 255)\n";
-   cin >> red;
   
-  } 
-
-
-
-  cout << "Enter a GREEN color value (Must be between 0 and 255)\n";
-  cin >> green;
-
-  while (green < 0 || green > 255){
-    cout << "Sorry, that number is not in the specified range. \n";
-   cout << "Please re-enter a GREEN color value (Must be between 0 and 255)\n";
-   cin >> green;
-  }
-
-  cout << "Enter a BLUE color value (Must be between 0 and 255)\n";
-  cin >> blue;
-   while (blue < 0 || blue > 255) {
-    cout << "Sorry, that number is not in the specified range. \n";
-   cout << "Please re-enter a BLUE color value (Must be between 0 and 255)\n";
-   cin >> blue;
-  }
-
-  // to do: figure out how to take these inputs and put them in a file
-
   cout << "Please enter a name for the file you will be storing the rgb values in:\n";
   cin >> filename;
 
-  reader.open(filename, ios::out);
-  ofstream outfile;
-  outfile.open(filename);
+  reader.open(filename);
 
   cout << "Writing to the file..." << endl << endl;
+  reader << "rgb1(" << red << ",";
+  reader << green << ",";
+  reader << blue << ")" << endl; 
 
-  outfile << red << endl;
-  outfile << green << endl;
-  outfile << blue << endl;
-  outfile.close();
+  reader.close();
 
 
   cout << "Would you like to read from the file? (y/n)\n";
-
+  string proceed;
   cin >> proceed;
   if (proceed == "y" || proceed == "Y"){
-
-  cout << "Reading from file..." << endl << endl;
-
-   ifstream infile; 
-   infile.open(filename);
-    infile >> red >> green >> blue;
-
-  
-   cout << "rgb(" << red << "," << green << "," << blue << ")" << endl << endl; 
-
-  infile.close();
+    cout << "Reading from file..." << endl << endl;
+    string line;
+    ifstream infile; 
+    infile.open(filename);
+    getline(infile, line);
+    cout << line << endl;
+    infile.close();
   }
+
   cout << "Now that it has been identified which color you would like as a base color, a list of five more colors that do not have problematic color combinations can be generated." << endl << endl;
 
-  cout << "Would you like to add five more colors to " << filename << "? (Enter y for yes and n for no)" << endl << endl;
-
+  cout << "Would you like to add five more colors to " << filename << "? (y/n)" << endl << endl;
+  string affirmative;
   cin >> affirmative;
+
+  int new_red[5] = {0, 0, 0, 0, 0};
+  int new_green[5] = {0, 0, 0, 0, 0};
+  int new_blue[5] = {0, 0, 0, 0, 0};
+
+  int sum = red + green + blue;
+  
+  //0-42    43-85  86-128  129-171 172-212 213-255
+  
+  //0-127  128-256 257-385 386-514 515-637 638-765
+
+  GenerateColors(sum, new_red, new_green, new_blue);
 
   if (affirmative == "y" || affirmative == "Y"){
 
-    reader.open(filename, ios::out);
-    ofstream outfile;
-    outfile.open(filename);
+    reader.open(filename, ofstream::app);
 
-     outfile << red2 << endl;
-     outfile << green2 << endl;
-     outfile << blue2 << endl;
-     outfile << red3 << endl;
-     outfile << green3 << endl;
-     outfile << blue3 << endl;
-     outfile << red4 << endl;
-     outfile << green4 << endl;
-     outfile << blue4 << endl;
-     outfile << red5 << endl;
-     outfile << green5 << endl;
-     outfile << blue5 << endl;
-     outfile << red6 << endl;
-     outfile << green6 << endl;
-     outfile << blue6 << endl;     
+    for(int i = 0; i < 5; i++){
+      reader << "rgb" << i+2 << "(" << new_red[i] << ",";
+      reader << new_green[i] << ",";
+      reader << new_blue[i] << ")" << endl; 
+      
+    }     
 
-     outfile.close();
+    reader.close();
 
   }
 
+  cout << "Printing out all six nonproblematic colors...\n";
+  cout << "rgb1(" << red << ", ";
+  cout << green << ", " << blue << ")\n";
+  cout << "rgb2(" << new_red[0] << ", ";
+  cout << new_green[0] << ", " << new_blue[0] << ")\n";
+  cout << "rgb3(" << new_red[1] << ", ";
+  cout << new_green[1] << ", " << new_blue[1] << ")\n";
+  cout << "rgb4(" << new_red[2] << ", ";
+  cout << new_green[2] << ", " << new_blue[2] << ")\n";
+  cout << "rgb5(" << new_red[3] << ", ";
+  cout << new_green[3] << ", " << new_blue[3] << ")\n";
+  cout << "rgb6(" << new_red[4] << ", ";
+  cout << new_green[4] << ", " << new_blue[4] << ")\n";
+
   return 0;
 }
+
+int CheckRGBValue(string colour){
+  int temporary = 0;
+  cout << "Enter a " << colour << " color value (Must be between 0 and 255)\n";
+  cin >> temporary;
+
+  while (temporary < 0 || temporary > 255){
+    cout << "Sorry, that number is not in the specified range. \n";
+    cout << "Please re-enter a " << colour << " color value (Must be between 0 and 255)\n";
+    cin >> temporary;
+  }
+  return temporary;
+}
+
+void GenerateColors(int sum, int (&new_red)[5], int (&new_green)[5], int (&new_blue)[5]){
+  
+  //0-42    43-85  86-128  129-171 172-212 213-255
+  
+  //0-127  128-256 257-385 386-514 515-637 638-765
+
+  if(sum >= 0 && sum <= 127){
+    new_red[0] = 102;
+    new_red[1] = 75;
+    new_red[2] = 145;
+    new_red[3] = 200;
+    new_red[4] = 234;
+
+    new_green[0] = 46;
+    new_green[1] = 155;
+    new_green[2] = 123;
+    new_green[3] = 108;
+    new_green[4] = 248;
+
+    new_blue[0] = 0;
+    new_blue[1] = 22;
+    new_blue[2] = 170;
+    new_blue[3] = 250;
+    new_blue[4] = 199;
+   
+  }
+  else if(sum >= 128 && sum <= 256){
+    new_red[0] = 0;
+    new_red[1] = 75;
+    new_red[2] = 145;
+    new_red[3] = 200;
+    new_red[4] = 234;
+
+    new_green[0] = 0;
+    new_green[1] = 155;
+    new_green[2] = 123;
+    new_green[3] = 108;
+    new_green[4] = 248;
+
+    new_blue[0] = 0;
+    new_blue[1] = 22;
+    new_blue[2] = 170;
+    new_blue[3] = 250;
+    new_blue[4] = 199;
+
+  }
+  else if(sum >= 257 && sum <= 385){
+    new_red[0] = 0;
+    new_red[1] = 102;
+    new_red[2] = 145;
+    new_red[3] = 200;
+    new_red[4] = 234;
+
+    new_green[0] = 0;
+    new_green[1] = 46;
+    new_green[2] = 123;
+    new_green[3] = 108;
+    new_green[4] = 248;
+
+    new_blue[0] = 0;
+    new_blue[1] = 0;
+    new_blue[2] = 170;
+    new_blue[3] = 250;
+    new_blue[4] = 199;
+
+  }
+  else if(sum >= 386 && sum <= 514){
+    new_red[0] = 0;
+    new_red[1] = 102;
+    new_red[2] = 75;
+    new_red[3] = 200;
+    new_red[4] = 234;
+
+    new_green[0] = 0;
+    new_green[1] = 46;
+    new_green[2] = 155;
+    new_green[3] = 108;
+    new_green[4] = 248;
+
+    new_blue[0] = 0;
+    new_blue[1] = 0;
+    new_blue[2] = 22;
+    new_blue[3] = 250;
+    new_blue[4] = 199;
+
+  }
+  else if(sum >= 515 && sum <= 637){
+    new_red[0] = 0;
+    new_red[1] = 102;
+    new_red[2] = 75;
+    new_red[3] = 145;
+    new_red[4] = 234;
+
+    new_green[0] = 0;
+    new_green[1] = 46;
+    new_green[2] = 155;
+    new_green[3] = 123;
+    new_green[4] = 248;
+
+    new_blue[0] = 0;
+    new_blue[1] = 0;
+    new_blue[2] = 22;
+    new_blue[3] = 170;
+    new_blue[4] = 199;
+
+  }
+  else if(sum >= 638 && sum <= 765){
+    new_red[0] = 0;
+    new_red[1] = 102;
+    new_red[2] = 75;
+    new_red[3] = 145;
+    new_red[4] = 200;
+
+    new_green[0] = 0;
+    new_green[1] = 46;
+    new_green[2] = 155;
+    new_green[3] = 123;
+    new_green[4] = 108;
+
+    new_blue[0] = 0;
+    new_blue[1] = 0;
+    new_blue[2] = 22;
+    new_blue[3] = 170;
+    new_blue[4] = 250;
+
+  }
+  
+}
+
